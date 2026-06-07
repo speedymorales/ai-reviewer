43e181f8fb460f8922ba8dd9fbe6c150dac44486
Jose Morales <speedymorales5@gmail.com>
2026-06-07 13:10:11 -0400

    removed payload logging

--- src/issue_comment.ts
+++ src/issue_comment.ts
@@ -1,15 +1,8 @@
 import { info, warning } from "@actions/core";
 import { loadContext } from "./context";
-import config from "./config";
-import { initOctokit } from "./octokit";
 import {
-  buildComment,
-  getCommentThread,
   isOwnComment,
-  isThreadRelevant,
 } from "./comments";
-import { parseFileDiff } from "./diff";
-import { runReviewCommentPrompt } from "./prompts";
 import { doPullRequestReview } from "./pull_request_reviewer";
 
 export async function handleIssueComments() {
@@ -19,7 +12,6 @@ export async function handleIssueComments() {
     return;
   }
 
-  info(`context payload: ${JSON.stringify(context.payload)}`);
   const { comment, issue, repository } = context.payload;
   if (!comment) {
     warning("`comment` is missing from payload");
@@ -43,68 +35,4 @@ export async function handleIssueComments() {
   }
 
   await doPullRequestReview(context, issue.number);
-
-  // const octokit = initOctokit(config.githubToken, config.githubApiUrl);
-
-  // const pullRequest = await octokit.rest.pulls.get({
-  //   ...context.repo,
-  //   pull_number: issue.number,
-  // })
-
-  // // Fetch comment thread
-  // const commentThread = await getCommentThread(octokit, {
-  //   ...context.repo,
-  //   pull_number: issue.number,
-  //   comment_id: comment.id,
-  // });
-  // if (!commentThread) {
-  //   warning("comment thread not found");
-  //   return;
-  // }
-
-  // // Check if the comment thread is relevant
-  // if (!isThreadRelevant(commentThread)) {
-  //   info("comment thread is not relevant, ignoring it");
-  //   return;
-  // }
-
-  // // Fetch diffs for all files
-  // const { data: files } = await octokit.rest.pulls.listFiles({
-  //   ...context.repo,
-  //   pull_number: issue.number,
-  // });
-  // let fileDiffs = files.map((file) => parseFileDiff(file, []));
-
-  // // Find the file that the comment is in
-  // const commentFileDiff = fileDiffs.find(
-  //   (fileDiff) => fileDiff.filename === commentThread.file
-  // );
-  // if (!commentFileDiff) {
-  //   warning("comment is not in any file that was changed in this PR");
-  //   return;
-  // }
-
-  // // Run prompt
-  // const response = await runReviewCommentPrompt({
-  //   commentThread,
-  //   commentFileDiff,
-  // });
-
-  // // Submit response if action requested
-  // if (!response.action_requested || !response.response_comment.length) {
-  //   info(
-  //     "comment doesn't seem to require any action, so not submitting a response"
-  //   );
-  //   return;
-  // }
-
-  // info("action requested, submitting response");
-  // await octokit.rest.pulls.createReviewComment({
-  //   ...context.repo,
-  //   pull_number: issue.number,
-  //   commit_id: pullRequest.data.head.sha,
-  //   path: commentThread.file,
-  //   body: buildComment(response.response_comment),
-  //   in_reply_to: commentThread.comments[0].id,
-  // });
 }
